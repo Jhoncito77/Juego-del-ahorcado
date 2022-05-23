6f04abe8bbdec452df3ba0313983e065c9cd2c20
Jhon Moreno <jhonjairomoreno90@gmail.com>
2022-05-23 08:43:30 -0500

    finalizacion segunda tarea backlog crear tablero de juego

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,13 @@
 /**
  * funcionalidades del proyecto Juego del ahoracado
  */
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+var botonIniciar = document.querySelector("#botoniniciar");
+
+botonIniciar.addEventListener("click",function(){
+    botonIniciar.classList.add("ocultar");
+    pantalla.classList.remove("ocultar");
+
+    
+});

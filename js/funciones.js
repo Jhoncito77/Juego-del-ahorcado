1290238119953c46532142f6053f9265993cba8a
Jhon Moreno <jhonjairomoreno90@gmail.com>
2022-05-23 09:16:01 -0500

    Se agrega funcion para elegir palabra aleatoria para el juego

--- js/funciones.js
+++ js/funciones.js
@@ -9,5 +9,21 @@ botonIniciar.addEventListener("click",function(){
     botonIniciar.classList.add("ocultar");
     pantalla.classList.remove("ocultar");
 
-    
+    let palabraSecreta = elegirPalabra();
+    console.log(palabraSecreta);
+
 });
+
+var palabras = ["HOLA", "QUE", "MAS", "COMO", "ESTA", "PC", "LORENA", "JUAN", "JHON", "MORENO"];
+
+function elegirPalabra(){
+    let i = Math.round(Math.random()*palabras.length)-1;
+    if(i<0){
+        i=0;
+    }
+    let palabraElegida = palabras[i];
+    console.log(i)
+    return palabraElegida;
+    
+}
+
